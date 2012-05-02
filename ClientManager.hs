fe6cea3db49b67cd9a7e64125cf2cb04c854c54b
Naoki Yoshinaga <thats_525@mail.goo.ne.jp>
2012-05-02 18:43:41 +0900

    fix a bug about foldl and IO

--- ClientManager.hs
+++ ClientManager.hs
@@ -45,44 +45,48 @@ resolveClientMessages server phiworld pcdb = do
 resolveClientProtocolResult ::
   NS.SimpleTCPServer -> [ClientProtocolResult] -> PhiWorld -> PCD.PlayerCharacterDB ->
   IO (PhiWorld, PCD.PlayerCharacterDB)
-resolveClientProtocolResult server result_list phiworld pcdb =
-  foldl (\current_both result -> do
-            ((phimap, ClientIDSet cidset, PcSet pcset), current_pcdb) <- current_both
-            case result of
-              NewPc cid phirc pc -> do
-                let new_cidset = addToAL cidset cid phirc
-                let new_pcset = addToAL pcset phirc pc
-                return ((phimap, ClientIDSet new_cidset, PcSet new_pcset), current_pcdb)
-              PcStatusChange phirc pc -> do
-                let new_pcset = addToAL pcset phirc pc
-                return ((phimap, ClientIDSet cidset, PcSet new_pcset), current_pcdb)
-              PrivateMessage cid msg -> do
-                -- ignore disconnected client
-                _ <- NS.sendMessageTo server cid msg
-                current_both
-              LogoutPc cid -> do
-                case lookup cid cidset of
-                  -- already disconnected
-                  Nothing -> current_both
-                  Just phirc -> do
-                    _ <- NS.sendMessageTo server cid $ PE.encodeProtocol PE.Close
-                    _ <- NS.disconnectClient server cid
-                    let maybe_pc = lookup phirc pcset
-                    case maybe_pc of
-                      Nothing -> error "Assertion error"
-                      Just pc -> do let new_pcdb = PCD.savePc pcdb phirc pc
-                                    let new_cidset = delFromAL cidset cid
-                                    let new_pcset = delFromAL pcset phirc
-                                    return((phimap, ClientIDSet new_cidset, PcSet new_pcset), new_pcdb)
-              ForceDisconnectByNoPc cid -> do
-                _ <- NS.sendMessageTo server cid $ PE.encodeProtocol PE.X
-                _ <- NS.disconnectClient server cid
-                current_both
-              ForceDisconnectByDupPc cid -> do
-                _ <- NS.sendMessageTo server cid $ PE.encodeProtocol PE.X
-                _ <- NS.disconnectClient server cid
-                current_both
-          ) (return (phiworld, pcdb)) result_list
+resolveClientProtocolResult server result_list phiworld first_pcdb = do
+  let (final_world, final_pcdb, final_io_list) =
+        foldl (\((phimap, ClientIDSet cidset, PcSet pcset), pcdb, io_list) result ->
+                case result of
+                  NewPc cid phirc pc ->
+                    let new_cidset = addToAL cidset cid phirc in
+                    let new_pcset = addToAL pcset phirc pc in
+                    ((phimap, ClientIDSet new_cidset, PcSet new_pcset), pcdb, io_list)
+                  PcStatusChange phirc pc ->
+                    let new_pcset = addToAL pcset phirc pc in
+                    ((phimap, ClientIDSet cidset, PcSet new_pcset), pcdb, io_list)
+                  PrivateMessage cid msg ->
+                    -- ignore disconnected client
+                    let io = NS.sendMessageTo server cid msg in
+                    ((phimap, ClientIDSet cidset, PcSet pcset), pcdb, io:io_list)
+                  LogoutPc cid ->
+                    case lookup cid cidset of
+                      -- already disconnected
+                      Nothing -> ((phimap, ClientIDSet cidset, PcSet pcset), pcdb, io_list)
+                      Just phirc ->
+                        let io1 = NS.sendMessageTo server cid $ PE.encodeProtocol PE.Close in
+                        let io2 = NS.disconnectClient server cid in
+                        let maybe_pc = lookup phirc pcset in
+                        case maybe_pc of
+                          Nothing -> error "Assertion error"
+                          Just pc -> 
+                            let new_pcdb = PCD.savePc pcdb phirc pc in
+                            let new_cidset = delFromAL cidset cid in
+                            let new_pcset = delFromAL pcset phirc in
+                            ((phimap, ClientIDSet new_cidset, PcSet new_pcset), 
+                             new_pcdb, io2:io1:io_list)
+                  ForceDisconnectByNoPc cid ->
+                    let io1 = NS.sendMessageTo server cid $ PE.encodeProtocol PE.X in
+                    let io2 = NS.disconnectClient server cid in
+                    ((phimap, ClientIDSet cidset, PcSet pcset), pcdb, io2:io1:io_list)
+                  ForceDisconnectByDupPc cid ->
+                    let io1 = NS.sendMessageTo server cid $ PE.encodeProtocol PE.X in
+                    let io2 = NS.disconnectClient server cid in
+                    ((phimap, ClientIDSet cidset, PcSet pcset), pcdb, io2:io1:io_list)
+          ) (phiworld, first_pcdb, []) result_list
+  mapM_ (\x -> do _ <- x; return ()) $ reverse final_io_list
+  return (final_world, final_pcdb)
 
 data ClientProtocolResult = NewPc NS.ClientID Phirc PC.PlayerCharacter
                           | PcStatusChange Phirc PC.PlayerCharacter
